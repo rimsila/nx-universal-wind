f3d5390c48b0d461a06bae3b50beed9ab95e40fb
zila <rimsila.itc@gmail.com>
2025-11-25 03:46:15 +0700

    build(deps): update expo metro dependencies and config

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,30 +1,18 @@
-const { withNxMetro } = require("@nx/expo");
 const { getDefaultConfig } = require("@expo/metro-config");
-const { mergeConfig } = require("metro-config");
 const { withNativewind } = require("nativewind/metro");
 
-const defaultConfig = getDefaultConfig(__dirname);
-const { assetExts, sourceExts } = defaultConfig.resolver;
+const config = getDefaultConfig(__dirname);
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const customConfig = {
+module.exports = withNativewind({
+  ...config,
   cacheVersion: "@counter-monorepo/mobile",
   transformer: {
+    ...config.transformer,
     babelTransformerPath: require.resolve("react-native-svg-transformer"),
   },
   resolver: {
-    assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "cjs", "mjs", "svg"],
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...config.resolver.sourceExts, "cjs", "mjs", "svg"],
   },
-};
-
-module.exports = withNxMetro(mergeConfig(defaultConfig, customConfig), {
-  debug: false,
-  extensions: [],
-  watchFolders: [],
-}).then((config) => withNativewind(config));
+});
